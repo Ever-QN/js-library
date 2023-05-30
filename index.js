8a7d36cf2005e9acf23a433a962565a0837074c2
Ever-QN <QuynhTNgo@live.com>
2023-05-30 10:56:27 -0600

    Add info method to book constructor

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ function Book(title, author, pages, read) {
   this.author = author,
   this.pages = pages,
   this.read = read
+  this.info = function() {
+    return this.title + " by " + this.author + ", " + this.pages + ", " + this.read;
+  }
 }
 
 function addBookToLibrary() {
   // do stuff here
 }
+
+// const theHobbit = new Book("The Hobbbit", "J.R.R Tolkien", "295 pages", "not read");
+// console.log(theHobbit.info());
